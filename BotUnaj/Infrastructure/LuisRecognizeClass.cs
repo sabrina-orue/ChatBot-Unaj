0d3bdd36f954c07e7987a52910f087d5c6860d23
ivan-emanuel-rodriguez <ivan-emanuel-rodriguez@users.noreply.github.com>
2020-06-15 23:32:59 -0300

    Bot Luis

---
+++ BotUnaj/Infrastructure/LuisRecognizeClass.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Bot.Builder.AI.Luis;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotUnaj.Infrastructure
+{
+    public class LuisRecognizeClass
+    {
+        public LuisRecognizer _recognizer { get; private set; }
+
+        public LuisRecognizeClass(IConfiguration configuration) //Constructor
+        {
+            var luisApplication = new LuisApplication( //Creo la aplicacion Luis
+                //Llamo a las claves de congiguracion creadas en appsettings
+                configuration["LuisAppId"],
+                configuration["LuisAPIKey"],
+                configuration["LuisHostName"]
+                );
+            var recognizerOptions = new LuisRecognizerOptionsV3(luisApplication) //Aca obtenemos las respuestas del servicio
+            {
+                PredictionOptions = new Microsoft.Bot.Builder.AI.LuisV3.LuisPredictionOptions()
+                {
+                    IncludeInstanceData = true
+                }
+            };
+
+            _recognizer = new LuisRecognizer(recognizerOptions); //Asigno las respuestas o resultados a recognizer
+       }
+    }
+}
---
+++ BotUnaj/Infrastructure/LuisRecognizeInterface.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Bot.Builder.AI.Luis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BotUnaj.Infrastructure
+{
+    public interface LuisRecognizeInterface
+    {
+        LuisRecognizer _recognizer { get; }
+    }
+}
